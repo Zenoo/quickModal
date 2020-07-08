0e0090be9507581d6ced10eac46a7de9fea6e36a
Zenoo <perso@franckdemoute.fr>
2020-07-08 10:33:33 +0200

    fix: Add a way to keep the modal on close

--- quickModal.js
+++ quickModal.js
@@ -9,6 +9,7 @@ class QuickModal{
 	 * @param {Object|String}           parameters                         Parameters holder. Use a String if you want a quick initialization
 	 * @param {Boolean}                 [parameters.darkenBackground=true] Should the QuickModal darken the background when shown?
      * @param {Boolean}                 [parameters.isForm=true]           Is the QuickModal a form?
+     * @param {Boolean}                 [parameters.keepHiden=false]       Keep the modal hidden instead of destroying it
      * @param {Object}                  [parameters.form]                  Form properties holder
      * @param {String}                  [parameters.form.action]           Form URL
      * @param {String}                  [parameters.form.method]           Form METHOD attribute (GET/POST/...)
@@ -35,6 +36,7 @@ class QuickModal{
 		this._parameters = {
 			darkenBackground: true,
 			isForm: true,
+			keepHiden: false,
 			form: {
 				action: 'path/to/your/form',
 				method: 'POST',
@@ -310,7 +312,7 @@ class QuickModal{
 		// Close action handling
 		const closingHandler = e => {
 			e.preventDefault();
-			this.destroy();
+			if(!this._parameters.keepHiden) this.destroy();
 		};
 
 		this._elements.hider.addEventListener('click', closingHandler);
